86d26c7137fa691e9dbdfe121f4a27d35b7f8266
husain Al-Mohssen <husain@alum.mit.edu>
2020-06-19 09:41:26 -0400

    fixed a few Docker/build issues

--- comp_1/build_comp.sh
+++ comp_1/build_comp.sh
@@ -6,6 +6,5 @@ export comp_name=`pwd | rev | cut -f1 -d/ |rev`
 export docker_name=$docker_user/$comp_name:$GIT_SHA
 
 
-echo $comp_name
 docker build -t $docker_name --build-arg comp_name=$comp_name  . &&
 docker push docker.io/$docker_name
