d00602bbab089eb55a9b77cff1de130d4e1c351c
husain Al-Mohssen <husain@alum.mit.edu>
2020-06-19 13:10:09 -0400

    printf typo

--- comp_2/__init__.py
+++ comp_2/__init__.py
@@ -11,5 +11,5 @@ def run(input1: float, random_str:str) -> float:
     print(f"running in comp_2, input1 was {input1} only works if I'm running in the clojure container")
     print(f"I'm doing nothing with the {random_str}")
     os.system('clj -e "(+ 1 1)"') # the clojure container has a python3 interperter installed 
-    printf(f"calling a function with external dependencies {eg.ex_func()}")
+    print(f"calling a function with external dependencies {eg.ex_func()}")
     return input1
