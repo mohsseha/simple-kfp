9586d354ee35779bba4bae8514d6d5e888c3d641
husain Al-Mohssen <husain@alum.mit.edu>
2020-06-19 12:56:04 -0400

    fixed dependency on comp_2

--- comp_1/__init__.py
+++ comp_1/__init__.py
@@ -1,9 +1,9 @@
 #WARNING: do not include any dependencies that would not work in a standard python3 installation 
 #(this is because the kfp pipeline builder does not have access to the packages defined in this docker image)
-
+from typing import NamedTuple
 def run(input1: float,input2: float) -> NamedTuple('Cmp1Output', [('input1', float), ('input2', float), ('result', float)]):
     # if you need external deps they should be imported here: 
-    from typing import NamedTuple
+    
     print(f"running in comp_1, input1 was {input1} and inptu 2= {input2}")
 
     from collections import namedtuple
--- comp_2/__init__.py
+++ comp_2/__init__.py
@@ -6,7 +6,7 @@ def run(input1: float, random_str:str) -> float:
     # all imports should be here: 
     from typing import NamedTuple
     import os
-    import example as eg # this can't be "seen" by the pipeline code 
+    import comp_2.example as eg # this can't be "seen" by the pipeline code 
 
     print(f"running in comp_2, input1 was {input1} only works if I'm running in the clojure container")
     print(f"I'm doing nothing with the {random_str}")
