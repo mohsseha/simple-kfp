ff1a0319a8e246b8f3b0133908dc190d4b8535f7
husain Al-Mohssen <husain@alum.mit.edu>
2020-06-19 09:22:12 -0400

    added comp build script

---
+++ comp_1/build_comp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker_user='mohsseha'
+GIT_SHA=`git rev-parse HEAD`
+export comp_name=`pwd | rev | cut -f1 -d/ |rev`
+export docker_name=$docker_user/$comp_name:$GIT_SHA
+
+
+echo $comp_name
+docker build -t $docker_name --build-arg comp_name=$comp_name  . &&
+docker push docker.io/$docker_name
